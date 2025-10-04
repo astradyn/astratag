91f4c907d190751588a64a4c7b4ae7f8ba5e3aba
ravikt <ravikt@tamu.edu>
2025-10-04 07:29:15 +0200

    preprocessing for quadrilateral detection

--- src/quadrilateral.cpp
+++ src/quadrilateral.cpp
@@ -18,4 +18,33 @@ cv::Mat QuadrilateralDetector::readImage(const std::string& imagePath)
 	return image;
 }
 
+cv::Mat QuadrilateralDetector::preprocessImage(const cv::Mat& image)
+{
+	cv::Mat gray;
+	cv::cvtColor(image, gray, cv::COLORBGR2GRAY);
 
+	// Apply CLAHE
+	cv::Ptr<cv::CLAHE> clahe = cv::createCLAHE(2.0, cv::Size(8,8));
+	cv::Mat enhanced;
+	clahe->apply(gray, enhanced);
+
+	cv::imwrite("enhanced.png", enhanced);
+	return enhanced;
+}
+
+cv::Mat QuadrilateralDetector::applyAdaptiveThreshold(const cv::Mat& gradientMagnitude)
+{
+	cv::Mat binary;
+	cv::adaptiveThreshold(gradientMagnitude, binary, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 11, 2);
+	return binary;
+}
+
+std::vector<std::vector<cv::Point>> QuadrilateralDetector::findContours(const cv::Mat& binary)
+{
+	std::vector<std::vector<cv::Point>> contours;
+	std::vector<cv::Vec4i> hierarchy;
+
+	cv::findContours(binary, contours, hierarchy, cv::RETR_CCOMP, cv::CHAIN_APPROX_NONE);
+	std::vector<std::vector<cv::Point>> filteredContours;
+
+}
